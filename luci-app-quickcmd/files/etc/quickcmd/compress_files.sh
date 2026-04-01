d3d4ab64e2d912a9c8c23113ccb685be3432decd
linwk24 <49717606+linwk24@users.noreply.github.com>
2026-04-01 17:28:41 +0800

    Delete luci-app-quickcmd directory

--- luci-app-quickcmd/files/etc/quickcmd/compress_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /mnt/sda1/fns
-for file in *; do
-    [ -f "$file" ] && tar -a -cf "${file%.*}.zip" "$file" && rm -f "$file"
-done
--- luci-app-quickcmd/readme.txt
+++
@@ -1,5 +0,0 @@
-方式1：手动部署（当前可用）
-
-cd /mnt/mmcblk0p28/tmp/luci-app-quickcmd
-chmod +x install.sh
-./install.sh
